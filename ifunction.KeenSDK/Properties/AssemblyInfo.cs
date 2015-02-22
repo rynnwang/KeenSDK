ad8bf1c4f318fafa91656fe745692571d3c62e6c
Rynn.Wang <rynn.wang@outlook.com>
2015-02-22 19:22:58 +0800

    nuget update

--- ifunction.KeenSDK/Properties/AssemblyInfo.cs
+++ ifunction.KeenSDK/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ifunction.KeenSDK")]
-[assembly: AssemblyDescription("3.1.3")]
+[assembly: AssemblyDescription("3.1.4")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("ifunction.KeenSDK")]
